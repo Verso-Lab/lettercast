78c9dc07fc3555d135b6d7be1e2db761b0814eeb
Kaveh Waddell <123405488+kavwad@users.noreply.github.com>
2025-01-31 10:22:27 -0800

    change run.py cli argument to `-m`

--- src/run.py
+++ src/run.py
@@ -6,13 +6,13 @@ from handler import lambda_handler
 if __name__ == "__main__":
     # Set up argument parser
     parser = argparse.ArgumentParser(description='Process recent podcast episodes')
-    parser.add_argument('--minutes', type=int, default=60,
+    parser.add_argument('-m', type=int, default=60,
                       help='Number of minutes to look back for new episodes (default: 60)')
     
     args = parser.parse_args()
     
     # Set environment variable for lambda_handler
-    os.environ['CHECK_MINUTES'] = str(args.minutes)
+    os.environ['CHECK_MINUTES'] = str(args.m)
     
     # Run the Lambda handler as if it was triggered by EventBridge
     result = asyncio.run(lambda_handler())
