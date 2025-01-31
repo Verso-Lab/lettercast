6c5a80d3a74dab1b8bdea7cbef08ccf802317f9c
Kaveh Waddell <123405488+kavwad@users.noreply.github.com>
2025-01-30 17:14:29 -0800

    add entry point

---
+++ src/run.py
@@ -0,0 +1,19 @@
+import asyncio
+import argparse
+import os
+from handler import lambda_handler
+
+if __name__ == "__main__":
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description='Process recent podcast episodes')
+    parser.add_argument('--minutes', type=int, default=60,
+                      help='Number of minutes to look back for new episodes (default: 60)')
+    
+    args = parser.parse_args()
+    
+    # Set environment variable for lambda_handler
+    os.environ['CHECK_MINUTES'] = str(args.minutes)
+    
+    # Run the Lambda handler as if it was triggered by EventBridge
+    result = asyncio.run(lambda_handler())
+    print(result) 
